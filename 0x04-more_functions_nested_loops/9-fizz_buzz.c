031e76bfe2d96977b5deb801f555cdc5ad65fbc3
Iheanacho Ekene Nicholas <93764522+DevNick21@users.noreply.github.com>
2022-09-15 15:53:46 +0100

    New Files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,24 +4,24 @@ A program that prints the numbers from 1 to 100, followed by a new line. But for
 */
 int main()
 {
-    for (int i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz ");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz ");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz ");
-        }
-        else
-        {
-            printf("%d ", i);
-        }
-    }
-    return 0;
+for (int i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+return 0;
 }
