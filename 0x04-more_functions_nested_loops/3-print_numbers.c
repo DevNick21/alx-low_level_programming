58d01f8f48c663164e718665ad3704027d2a2b6e
Iheanacho Ekene Nicholas <93764522+DevNick21@users.noreply.github.com>
2022-09-15 15:44:58 +0100

    New Files

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,11 @@
  */
 void print_numbers(void)
 {
-    	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i + '0');
-	}
-	_putchar('\n');
-    return (0);
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+return (0);
 }
