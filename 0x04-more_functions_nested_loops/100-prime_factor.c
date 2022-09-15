b2b2dcf0364d02ec90c4462b1c36b5df63c223d9
Iheanacho Ekene Nicholas <93764522+DevNick21@users.noreply.github.com>
2022-09-15 15:58:36 +0100

    New Files

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
+
 /**
- * main -  calculate largest prime of 612852475143
- *
- * Return: Success Always
- */
+* main - Finds and prints the largest prime
+*        factor of the number 612852475143.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    long int x = 612852475143;
-    long int py;
-
-    for (py = 2; py < x; py++)
-    {
-        if (x % py == 0)
-        {
-            x = x / py;
-        }
-    }
-    printf("%ld\n", py);
-    return (0);
+long prime = 612852475143, div;
+while (div < (prime / 2))
+{
+if ((prime % 2) == 0)
+{
+prime /= 2;
+continue;
+}
+for (div = 3; div < (prime / 2); div += 2)
+{
+if ((prime % div) == 0)
+prime /= div;
+}
+}
+printf("%ld\n", prime);
+return (0);
 }
