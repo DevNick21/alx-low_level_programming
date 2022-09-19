40a21fc64fc08b781acc0c42330eff8f1548c193
Iheanacho Ekene Nicholas <93764522+DevNick21@users.noreply.github.com>
2022-09-19 22:16:40 +0100

    Correction

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*main - check the code
-*
-*Return: Always 0.
-*/
-int _strlen(char *s){
-int l = 0;
-while (*s != '\0')
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
 {
-s++;
-l++;
-}
-return (l);
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
 }
