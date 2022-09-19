a24f3a47b7c5e68cacf280453e2fde666216f018
Iheanacho Ekene Nicholas <93764522+DevNick21@users.noreply.github.com>
2022-09-19 22:19:55 +0100

    Correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
+  * print_rev - prints string
+  * @s: input string
+  * Return: no return
+ */
+
 void print_rev(char *s)
 {
-int count = 0;
-while (*s != '\0')
-{
-s++;
-count++;
-}
-while (count > 0)
-{
-s--;
-_putchar(*s);
-count--;
-}
-_putchar('\n');
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+
+	while (count > 0)
+	{
+		s--;
+		_putchar(*s);
+		count--;
+	}
+	_putchar('\n');
 }
