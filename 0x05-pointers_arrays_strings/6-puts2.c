1d90ec43984167ab5ad7174df91766106b36fe74
Iheanacho Ekene Nicholas <93764522+DevNick21@users.noreply.github.com>
2022-09-19 22:25:06 +0100

    Correction

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,23 @@
 #include "main.h"
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
+ * puts2 - prints one char out of 2
+ * @str: type
+ *
+ */
+
 void puts2(char *str)
 {
-int i, j = 0;
-while (str[i] != '\0')
-{
-i++;
-}
-while (j < i)
-{
-_putchar(str[j]);
-j = j + 2;
-}
-_putchar('\n');
+	int i, j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j = j + 2;
+	}
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
-* main - check the code for
-*
-* Return: Always 0.
-*/
+ * print_array - print n of int array
+ * @a: pointer
+ * @n: n
+ */
 void print_array(int *a, int n)
 {
-short c = 0;
-while (n-- > 0)
-{
-printf("%d", a[c++]);
-if (n != 0)
-printf(", ");
-}
-printf("\n");
+	short c = 0;
+
+	while (n-- > 0)
+	{
+		printf("%d", a[c++]);
+		if (n != 0)
+			printf(", ");
+	}
+
+		printf("\n");
 }
