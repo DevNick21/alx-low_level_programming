3561748bb1650a7d917dc993a050f59449ac058a
Iheanacho Ekene Nicholas <93764522+DevNick21@users.noreply.github.com>
2022-10-04 11:13:35 +0100

    Coding style changes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,13 +19,13 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 
 	length1 = 0;
-	while ( * (s1 + length1) != '\0')
+	while (*(s1 + length1) != '\0')
 	{
 		length1++;
 	}
 
 	length2 = 0;
-	while ( * (s2 + length2) != '\0')
+	while (*(s2 + length2) != '\0')
 	{
 		length2++;
 	}
